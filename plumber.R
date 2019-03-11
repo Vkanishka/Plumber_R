2b108359bdd5452a60bf792b53f02e43e47d3799
Kanishka Venishetty <kanirules5@gmail.com>
2019-03-11 12:39:59 +0000

    Update plumber.R

--- plumber.R
+++ plumber.R
@@ -47,3 +47,17 @@ function() {
   rand <- rnorm(20)
   hist(rand)
 }
+
+
+#* Return the sum of two numbers
+#* @param a The first number to add
+#* @param b The second number to add
+#* @post /sum 
+function(a,b){
+  as.numeric(a) + as.numeric(b)
+}
+
+#* @get /sub
+substract <- function(a, b){
+  as.numeric(a) - as.numeric(b)
+}
