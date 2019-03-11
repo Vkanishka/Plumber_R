0383772adffd4a643f48c055ed5cc45adb2cd214
Kanishka Venishetty <kanirules5@gmail.com>
2019-03-11 11:53:20 +0000

    Initial Commit

--- plumber.R
+++ plumber.R
@@ -5,3 +5,13 @@
 library(plumber)
 
 
+#* @apiTitle Testing plumber API
+# (#* @api Swagger annotations)
+
+#' Echo the parameter that was sent in
+#' @param msg The message to echo back.
+#' @get /echo
+function(msg=""){
+  list(msg = paste0("The message is: '", msg, "'"))
+}
+
