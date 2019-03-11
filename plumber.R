38e515312d5024d8e5bb9611b0499034dfa319db
Kanishka Venishetty <kanirules5@gmail.com>
2019-03-11 12:00:12 +0000

    Update plumber.R

--- plumber.R
+++ plumber.R
@@ -15,3 +15,35 @@ function(msg=""){
   list(msg = paste0("The message is: '", msg, "'"))
 }
 
+#* Plot a Histogram
+#* @png
+#* @get /plot
+function() {
+  rand <- rnorm(100)
+  hist(rand)
+}
+
+#* updating an existing histogram
+#* @png
+#* @put /plot
+function() {
+  rand <- rnorm(50)
+  hist(rand)
+}
+
+
+#* adding a new plot to the system
+#* @png
+#* @post /plot
+function() {
+  rand <- rnorm(200)
+  hist(rand)
+}
+
+#* deleteing a new plot from the system
+#* @png
+#* @delete /plot
+function() {
+  rand <- rnorm(20)
+  hist(rand)
+}
